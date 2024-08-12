39469e437d3187ad44a00fe2f297970aa418c058
OgreLemonSoup <wonderwhyami@gmail.com>
2024-08-12 19:46:23 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,4 +36,4 @@ async def delete_file(request):
         return web.Response(status=500, text="Internal server error")
 
 WEB_DIRECTORY = "./js"
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+__all__ = ["WEB_DIRECTORY"]
